4cc47aec540100a53f2350b352770ff84b7411cf
Jesse Feng <fxm0016@126.com>
2021-04-18 13:54:16 +0800

    Create escape.js

---
+++ src/escape.js
@@ -0,0 +1,14 @@
+const encodeReserveRE = /[!'()*]/g;
+const encodeReserveReplacer = c => `%${c.charCodeAt(0).toString(16)}`;
+
+/**
+ * fixed encodeURIComponent which is more conformant to RFC3986:
+ * - escapes [!'()*]
+ *
+ * @param {string} str
+ * @returns {string}
+ */
+export default function escape(str) {
+  return encodeURIComponent(str)
+    .replace(encodeReserveRE, encodeReserveReplacer);
+}
