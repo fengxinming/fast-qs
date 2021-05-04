e841121882bd6092e0521b016b0d6ea1df03e3fa
fengxinming <fxm0016@126.com>
2021-05-04 14:44:06 +0800

    refactor: 💡 index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,13 +1,13 @@
 export declare function _parse(
 	str: string,
-	sep: string, 
-	eq: string, 
+	sep: string,
+	eq: string,
 	decode: (val: string) => string,
 	callback: (key: string, val: string) => any
 ): void
 
 export declare function _stringify(
-	obj: object, 
+	obj: object,
 	encode: (val: string) => string,
 	callback: (ret: string[], key: string, eq: any, val: string) => any
 ): void
@@ -18,17 +18,17 @@ export declare function unescape(str: string): string
 
 export declare function parse(
 	str: string,
-	sep?: string, 
-	eq?: string, 
+	sep?: string,
+	eq?: string,
 	options?: { decodeURIComponent?: Function }
 ): object
 
 export { parse as decode }
 
 export declare function stringify(
-	obj: object, 
-	sep?: string, 
-	eq?:string, 
+	obj: object,
+	sep?: string,
+	eq?: string,
 	options?: { encodeURIComponent?: Function }
 ): string
 
